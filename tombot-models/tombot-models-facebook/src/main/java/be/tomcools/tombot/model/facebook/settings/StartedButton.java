d0dfccc35f348343145852f5a02e6f9b03679d64
tomco <tom.cools@live.be>
2016-11-27 19:15:06 +0100

    Added StartedButton Getters.

--- tombot-models/tombot-models-facebook/src/main/java/be/tomcools/tombot/model/facebook/settings/StartedButton.java
+++ tombot-models/tombot-models-facebook/src/main/java/be/tomcools/tombot/model/facebook/settings/StartedButton.java
@@ -1,8 +1,11 @@
 package be.tomcools.tombot.model.facebook.settings;
 
+import lombok.Data;
+
 import java.util.Arrays;
 import java.util.List;
 
+@Data
 public class StartedButton {
     private String setting_type = "call_to_actions";
     private String thread_state = "new_thread";
